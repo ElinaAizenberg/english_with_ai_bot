7b0489f67d6fbfe505cecf3b9f157a15ede6c24c
Elina Aizenberg <77394738+ElinaAizenberg@users.noreply.github.com>
2024-06-02 11:53:28 +0900

    Update config.py

--- bot/config_data/config.py
+++ bot/config_data/config.py
@@ -30,19 +30,35 @@ class Config:
 
 def load_config(path: str | None = None) -> Config:
     """Load configuration data from .env file and create a dataclass."""
-    env = Env()
-    env.read_env(path)
-    return Config(
-        tg_bot=TgBot(
-            token=env('BOT_TOKEN'),
-            openai_api=env('API_KEY'),
-            admin_ids=list(map(int, env.list('ADMIN_IDS')))
-        ),
-        db=DataBase(
-            host=env('HOST'),
-            database=env('DATABASE'),
-            port=env('PORT'),
-            user=env('DB_USER'),
-            password=env('PASSWORD'),
+    if path:
+        env = Env()
+        env.read_env(path)
+        return Config(
+            tg_bot=TgBot(
+                token=env('BOT_TOKEN'),
+                openai_api=env('API_KEY'),
+                admin_ids=list(map(int, env.list('ADMIN_IDS')))
+            ),
+            db=DataBase(
+                host=env('HOST'),
+                database=env('DATABASE'),
+                port=env('PORT'),
+                user=env('DB_USER'),
+                password=env('PASSWORD'),
+            )
         )
-    )
+    else:
+        return Config(
+            tg_bot=TgBot(
+                token=os.getenv('BOT_TOKEN'),
+                openai_api=os.getenv('API_KEY'),
+                admin_ids=[int(os.getenv('ADMIN_IDS'))]
+            ),
+            db=DataBase(
+                host=os.getenv('HOST'),
+                database=os.getenv('DATABASE'),
+                port=os.getenv('PORT'),
+                user=os.getenv('DB_USER'),
+                password=os.getenv('PASSWORD'),
+            )
+        )
